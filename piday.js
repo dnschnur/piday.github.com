07dea439c780b97000b06d12419d1d211c93cd55
David Schnur <dnschnur@gmail.com>
2014-10-19 16:02:40 -0700

    Ultra-lightweight lightbox implementation.

--- piday.js
+++ piday.js
@@ -1,11 +1,5 @@
 $(function() {
 
-  var form = $('form');
-  var inputs = form.find(':input');
-  var required = inputs.filter('[required]');
-  var submit = inputs.filter('[type=submit]');
-  var confirmation = form.find('.confirmation');
-
   // On touch devices, the links with drop-downs aren't normally hoverable, so
   // we need to disable their action to allow them to expand the drop-down only.
   if ('ontouchstart' in document.documentElement) {
@@ -13,6 +7,38 @@ $(function() {
       return false;
     });
   }
+
+  // Very simple lightbox; assumes element has an img, adds a -full suffix.
+  var lightboxContent = $('<div class="lightbox-content"></div>');
+  var lightbox = $('<div class="lightbox"></div>')
+    .append(lightboxContent)
+    .appendTo('body')
+    .click(function() {
+      lightbox.fadeOut();
+    });
+
+  $('.lightboxable').each(function() {
+    var source = $(this);
+    var smallImage = source.children('img').prop('src');
+    var fullImage = smallImage.replace('.jpg', '-full.jpg');
+    source.on({
+      mouseover: function() {
+        var preload = new Image();
+        preload.src = fullImage;
+      },
+      click: function() {
+        lightboxContent.css('background-image', 'url(' + fullImage + ')');
+        lightbox.fadeIn();
+      }
+    });
+  });
+
+  // Very simple AJAX submission to Google Forms, with required validation
+  var form = $('form');
+  var inputs = form.find(':input');
+  var required = inputs.filter('[required]');
+  var submit = inputs.filter('[type=submit]');
+  var confirmation = form.find('.confirmation');
 
   // Primitive form validation; inform immediately on missing a required field
   required.blur(function() {
